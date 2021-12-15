56622c3e9c50e9bcfda7c1ea86d7085915d9dd08
oxedions <oxedions@gmail.com>
2021-12-15 08:26:06 +0000

    Doc: Add note for disklessset build in Ubuntu

--- packages.sh
+++ packages.sh
@@ -9,6 +9,7 @@ tar cvzf bluebanquise-bootset-$bluebanquise_bootset_version.tar.gz bluebanquise-
 rpmbuild -ta bluebanquise-bootset-$bluebanquise_bootset_version.tar.gz --define "_software_version $bluebanquise_bootset_version" --target=noarch
 rm -Rf bluebanquise-bootset-$bluebanquise_bootset_version bluebanquise-bootset-$bluebanquise_bootset_version.tar.gz
 
+#python3 -c "import site; print(site.getsitepackages()[0])"
 cp -a bluebanquise-disklessset bluebanquise-disklessset-$bluebanquise_disklessset_version
 tar cvzf bluebanquise-disklessset-$bluebanquise_disklessset_version.tar.gz bluebanquise-disklessset-$bluebanquise_disklessset_version
 rpmbuild -ta bluebanquise-disklessset-$bluebanquise_disklessset_version.tar.gz --define "_software_version $bluebanquise_disklessset_version" --target=noarch
